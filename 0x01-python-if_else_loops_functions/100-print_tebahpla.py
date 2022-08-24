d11de827d5d016df0f672f23c24977ded544a35c
Ephraim <talk2osseephsseephraim@gmail.com>
2022-08-24 14:51:02 -0700

    go

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for c in reversed(range(97, 123)):
-    if ord(chr(c)) % 2 == 0:
-        print("{}".format(upper(chr(c))), end="")
-    else:
-        print("{}".format(chr(c)), end="")
+# Author -Bamidele Adefolaju
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
